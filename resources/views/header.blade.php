ce4add7a94cf0cd169b927c0363c32d0ad0a0e6e
Fraion Hyudz Esguerra <107916082+Hyudz@users.noreply.github.com>
2024-03-07 09:14:15 +0800

    Update header.blade.php

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -4,7 +4,7 @@
         <a href="{{route('landing')}}">
             <img src="{{asset('img/medyo final na logo 1.png')}}" style="height: 10vmin;" alt="" loading="lazy" class="m-2"/>
         </a>
-        <form class="d-md-flex input-group w-auto my-auto" method="POST" action="{{route('search2')}}">
+        <form class="d-md-flex input-group w-auto my-auto" method="POST" action="{{secure_url(route('search2'))}}">
             @csrf
             <input type="search" name="search" class="form-control" placeholder='I am looking for...' style="min-width: 300px; border-radius: 60px 0px 0px 60px"/>
             <span class="input-group-text border-0">
@@ -18,7 +18,7 @@
     <ul class="navbar-nav ms-auto">
         <div class="container">
             <span style="color: white;">Log in or</span> <a href="{{route('signup')}}" style="text-decoration: underline; color: #D3B306;"> Create Your Marketplace Account!</a>
-            <form class="d-flex me-5" action="{{route('login-post')}}" method="POST">
+            <form class="d-flex me-5" action="{{secure_url(route('login-post'))}}" method="POST">
                 @csrf
                 <input class="form-controll me-2" type="email" name="email" placeholder="Email">
                 <input class="form-controll me-2" type="password" name="password" placeholder="Password">
