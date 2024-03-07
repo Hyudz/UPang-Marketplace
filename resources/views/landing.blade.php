fa80f1c4d3bf559a7e2314f027efab596eaa159c
Fraion Hyudz Esguerra <107916082+Hyudz@users.noreply.github.com>
2024-03-07 08:08:30 +0800

    Update landing.blade.php

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -7,7 +7,7 @@
     <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous"> -->
     <script src="https://kit.fontawesome.com/de52212229.js" crossorigin="anonymous"></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="{{asset('css/header.css')}}">
+<!--     <link rel="stylesheet" href="{{asset('css/header.css')}}"> -->
     <link rel="icon" href="{{asset('img/medyo final na logo 1.png')}}" type="image/x-icon">
 </head>
 <body>
